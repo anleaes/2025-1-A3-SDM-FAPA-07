b830d42d9613069e2fe9cd633e2f485fd604e20c
Victorpa12 <victor.paltnetter@hotmail.com>
2025-06-04 19:43:15 -0300

    atualização da classe serializer do servico

--- manutencaoapp/apps/servicos/serializers.py
+++ manutencaoapp/apps/servicos/serializers.py
@@ -1,2 +1,8 @@
 from models import Servico
 from rest_framework import serializers
+
+class ServicoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Servico
+        fields = '__all__'
+        read_only_fields = ['id']
