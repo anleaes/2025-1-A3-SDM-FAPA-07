544c9335efd577c618f56e51f745290138b0a990
Victorpa12 <victor.paltnetter@hotmail.com>
2025-06-05 18:13:37 -0300

    modificação no urls.py

--- manutencaoapp/apps/endereco/urls.py
+++ manutencaoapp/apps/endereco/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from rest_frameworks import routers
 from . import views
+
+app_name = 'endereco'
+
+router = routers.DefaultRouter()
+router.register('', views.EnderecoViewSet, basename='endereco')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
