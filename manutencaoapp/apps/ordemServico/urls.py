b7bb1a1214445942b58a7e1b4cd6083d94f37516
Victorpa12 <victor.paltnetter@hotmail.com>
2025-06-05 20:44:32 -0300

    construção urls

--- manutencaoapp/apps/ordemServico/urls.py
+++ manutencaoapp/apps/ordemServico/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path
 from rest_framework import routers
 from . import views
+
+app_name = 'ordemServico'
+
+router = routers.DefaultRouter()
+router.register('', views.OrdemServicoViewSet, basename='ordemServico')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
