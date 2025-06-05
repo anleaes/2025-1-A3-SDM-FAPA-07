d6cc89a8f3940a51273d1bd08806a1cc0974f08a
Victorpa12 <victor.paltnetter@hotmail.com>
2025-06-05 20:29:13 -0300

    criação models

---
+++ manutencaoapp/apps/ordemServico/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from cliente.models import Cliente
+from endereco.models import Endereco
+from tecnico.models import Tecnico
+from tipoDeServico.models import TipoDeServico
+from servicos.models import Servicos
+
+# Create your models here.
