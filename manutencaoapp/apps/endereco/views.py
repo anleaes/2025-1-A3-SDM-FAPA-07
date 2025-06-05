c1ffefd5c604fa40331591e10a821cbc8c97c1c4
Victorpa12 <victor.paltnetter@hotmail.com>
2025-06-05 18:04:47 -0300

    ajuste nas views

--- manutencaoapp/apps/endereco/views.py
+++ manutencaoapp/apps/endereco/views.py
@@ -3,3 +3,10 @@ from .models import Endereco
 from .serializer import EnderecoSerializer
 
 # Create your views here.
+
+class EnderecoViewSet(viewsets.ModelViewSet):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer
+
+    def get_queryset(self):
+        return self.queryset.order_by('id')
