0524aae94778312de116b3e4727fcabd0de21f12
Victorpa12 <victor.paltnetter@hotmail.com>
2025-06-05 01:30:03 -0300

    criação do models

--- manutencaoapp/apps/tecnico/models.py
+++ manutencaoapp/apps/tecnico/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Tecnico(models.Model):
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    especialidade = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Técnico'
+        verbose_name_plural = 'Técnicos'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.nome
