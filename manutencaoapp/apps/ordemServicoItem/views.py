4dee6a33602324339ee1683780831fc8259c59d3
Victorpa12 <victor.paltnetter@hotmail.com>
2025-06-05 23:21:33 -0300

    feito o import nas views

--- manutencaoapp/apps/ordemServicoItem/views.py
+++ manutencaoapp/apps/ordemServicoItem/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import OrdemServicoItem
+from .serializer import OrdemServicoItemSerializer
 
 # Create your views here.
