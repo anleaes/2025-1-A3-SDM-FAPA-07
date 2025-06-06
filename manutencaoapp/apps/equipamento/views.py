78920702093f18b79797f000f7890939dfd7b898
Victorpa12 <victor.paltnetter@hotmail.com>
2025-06-06 02:03:37 -0300

    criado as views

--- manutencaoapp/apps/equipamento/views.py
+++ manutencaoapp/apps/equipamento/views.py
@@ -4,3 +4,10 @@ from .models import Equipamento
 from .serializer import EquipamentoSerializer
 
 # Create your views here.
+
+class EquipamentoViewSet(viewsets.ModelViewSet):
+    queryset = Equipamento.objects.all()
+    serializer_class = EquipamentoSerializer
+
+    def get_queryset(self):
+        return self.queryset.order_by('id')
