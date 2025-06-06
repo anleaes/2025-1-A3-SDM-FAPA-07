bb467ece04c808e0cc2b474eaa9db641d3141791
Victorpa12 <victor.paltnetter@hotmail.com>
2025-06-05 23:18:15 -0300

    criação dos serializers

--- manutencaoapp/apps/ordemServicoItem/serializer.py
+++ manutencaoapp/apps/ordemServicoItem/serializer.py
@@ -1,2 +1,8 @@
 from .models import OrdemServicoItem
 from rest_framework import serializers
+
+class OrdemServicoItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrdemServicoItem
+        fields = '__all__'
+        read_only_fields = ['id']
