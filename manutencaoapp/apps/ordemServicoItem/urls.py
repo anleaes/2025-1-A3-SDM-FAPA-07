d90f7febde45638b211148ac58fefec8b7ca4457
Victorpa12 <victor.paltnetter@hotmail.com>
2025-06-05 23:26:01 -0300

    criada a url

--- manutencaoapp/apps/ordemServicoItem/urls.py
+++ manutencaoapp/apps/ordemServicoItem/urls.py
@@ -2,3 +2,11 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
+app_name = 'ordem_servico_item'
+
+router = routers.DefaultRouter()
+router.register('', views.OrdemServicoItemViewSet, basename='ordemservicoitem')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
