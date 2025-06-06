a115cd2e5fe3d1a836fdcdb36d9497866bfda795
Victorpa12 <victor.paltnetter@hotmail.com>
2025-06-05 23:21:51 -0300

    criada as views

--- manutencaoapp/apps/ordemServicoItem/views.py
+++ manutencaoapp/apps/ordemServicoItem/views.py
@@ -4,3 +4,10 @@ from .models import OrdemServicoItem
 from .serializer import OrdemServicoItemSerializer
 
 # Create your views here.
+
+class OrdemServicoItemViewSet(viewsets.ModelViewSet):
+    queryset = OrdemServicoItem.objects.all()
+    serializer_class = OrdemServicoItemSerializer
+
+    def get_queryset(self):
+        return self.queryset.order_by('id')
