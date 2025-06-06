11a00594bf48d0709a272080134176e32523a78e
Victorpa12 <victor.paltnetter@hotmail.com>
2025-06-06 01:57:56 -0300

    criação classe equipamento

---
+++ manutencaoapp/apps/equipamento/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class EquipamentoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'equipamento'
+    verbose_name = 'Equipamentos'
---
+++ manutencaoapp/apps/equipamento/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+from cliente.models import Cliente
+# Create your models here.
